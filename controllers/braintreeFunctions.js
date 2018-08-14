5524fad832fd0b05d0e7ccb92ec1ef53e4ebf02c
ays14 <ayush.sh143@gmail.com>
2018-08-14 23:58:49 +0530

    Add customer and payment functions

--- controllers/braintreeFunctions.js
+++ controllers/braintreeFunctions.js
@@ -7,4 +7,94 @@ const gateway = braintree.connect({
 	privateKey:'b0eb62dc8d3b72715f590e8524a89daf'
 });
 
-//* Create Payment Method
+const payment = {
+	create: gateway.paymentMethod.create({
+			/* 
+			! get customerId by creating a user
+			*/
+			customerId: '',
+			paymentMethodNonce: nonceFromTheClient
+			}, (err, result) => {
+				if (result) {
+					res.send(result);
+				} else {
+					res.status(500).send(error);
+				}
+		}), //? after creating payment method => transaction.sale() cane be executed
+	
+	update: gateway.paymentMethod.update('token', {
+			/* 
+			! has several options, see docs
+			*/
+		}),
+
+	find: 	gateway.paymentMethod.find('token', (err, paymentMethod) => {
+			/* 
+			? returns a 'notFoundError' / PaymentMethod Response Object
+			*/
+			if (paymentMethod) {
+				res.send(paymentMethod);
+			} else {
+				res.status(500).send(error);
+			}
+		}),
+
+	delete: gateway.paymentMethod.delete('token', (err) => {
+			/* 
+			? delete else throw notFoundError
+			*/
+			if (err) {
+				res.status(500).send(error);
+			}
+		})
+}
+
+const customer = {
+	create:	gateway.customer.create({
+			/* 
+			! has several options, see docs
+			*/
+			firstName: 'John',
+			lastName: 'Smith'
+			}, (err, result) => {
+			if (result) {
+				let resp = {
+					"success": result.success,
+					"customerId": result.customer.id
+				}
+				res.send(resp);
+			} else {
+				res.status(500).send(error);
+			}
+		}),
+
+	update:	gateway.customer.update('customerId', {
+			/* 
+			? give update parameters and result in callback returns updated value
+			*/
+			}, (err, result) => {
+			if (result) {
+				res.send(result);
+			} else {
+				res.status(500).send(error);
+			}
+		}),
+
+	find:	gateway.customer.find('customerid', (err, customer) => {
+			/* 
+			? returns customer response object else notFoundError
+			*/
+			if (customer) {
+				res.send(customer);
+			} else {
+				res.status(500).send(error);
+			}
+		}),
+	
+	delete:	gateway.customer.delete("theCustomerId", function (err) {
+			if (err) {
+				res.status(500).send(error);
+			}
+			
+	  	})
+}
